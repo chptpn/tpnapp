a72e98194a6605c3b1bea3c51617afddb43a5157
brmac0302 <brmac0302@gmail.com>
2017-02-28 11:24:56 -0500

    publish: Update
    generated from commit 09f87b68cd9cfe55f4082b0161e2c17706ad0a4d

--- tpnapp/src/js/example-smart-app.js
+++ tpnapp/src/js/example-smart-app.js
@@ -24,7 +24,7 @@
         var meds = smart.patient.api.fetchAll({
                     type: 'MedicationOrder',
                     query: {
-                      status: {"active"}
+                      status: "active"
                     }
         });
 
