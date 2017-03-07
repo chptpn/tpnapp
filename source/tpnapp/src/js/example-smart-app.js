84cfc8e7aa2efa2ae3ab4d4a23ec7f9b55fef363
brmac0302 <brmac0302@gmail.com>
2017-03-07 08:38:01 -0500

    Updated tpnapp for weight

--- source/tpnapp/src/js/example-smart-app.js
+++ source/tpnapp/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|3141-9']
                       }
                     }
                   });
@@ -56,6 +57,7 @@
           }
 
           var height = byCodes('8302-2');
+          var weight = byCodes('3141-9');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -138,6 +140,7 @@
       birthdate: {value: ''},
       age: {value: ''},
       height: {value: ''},
+      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
@@ -230,6 +233,7 @@
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
     $('#age').append(p.age);
+    $('#weight').append(p.weight);
     /*$('#height').html(p.height);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
