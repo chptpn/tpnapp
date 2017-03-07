c3ef48f6a9b326aa78fe997bea1f1cf73e569c77
brmac0302 <brmac0302@gmail.com>
2017-03-07 09:10:41 -0500

    publish: Updated tpnapp for weight
    generated from commit 0e46d41cd32ae4c901f665b057272e273bd59ad3

--- tpnapp/src/js/example-smart-app.js
+++ tpnapp/src/js/example-smart-app.js
@@ -71,6 +71,8 @@
           p.lname = lname;
           p.age = parseInt(calculateAge(dob));
           p.height = getQuantityValueAndUnit(height[0]);
+          p.weight = getQuantityValueAndUnit(weight[0]);
+          p.weight2 = getQuantityValueAndUnit(weight2[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
